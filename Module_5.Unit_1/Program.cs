74f4bd7499e69eb45acf9f9ce65fd489a58704e7
DeniValuev <valuev.denis91@mail.ru>
2024-06-16 12:31:58 +0300

    Module_5-master: Add new Unit_1 in project

--- Module_5.Unit_1/Program.cs
+++ Module_5.Unit_1/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Задание 1
+            (string Name, string[] Dishes) User;
+            Console.WriteLine("Введите имя: ");
+            User.Name = Console.ReadLine();
+            User.Dishes = new string[5];
+            Console.WriteLine("Введите пять любимых блюда");
+            for (int i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.WriteLine("Блюдо {0}", i + 1);
+                User.Dishes[i] = Console.ReadLine();
+            }
+
         }
     }
 }
